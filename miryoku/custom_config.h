e40822bc09fd4b2fb892fa4545f9a15081099e7f
Schallabajzr <zan.schaffer@gmail.com>
2025-03-08 09:48:40 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -4,11 +4,11 @@
 #define XXX &none
 #define ULK &studio_unlock
 
-#define  MIRYOKU_LAYER_EURKEY \
-&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
-U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, &macro_eurkey S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
-U_LT(U_BUTTON, &macro_eurkey Z), U_MT(RALT, X),     &macro_eurkey C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+#define MIRYOKU_LAYER_EURKEY                                          \
+    XXX, XXX, XXX, XXX, XXX, XXX, XXX, XXX, XXX, XXX,                 \
+        XXX, XXX, &macro_eurkey S, XXX, XXX, XXX, XXX, XXX, XXX, XXX,             \
+        &macro_eurkey Z, XXX, &macro_eurkey C, XXX, XXX, XXX, XXX, XXX, XXX, XXX, \
+        XXX, XXX, XXX, XXX, XXX, XXX, XXX, XXX, XXX, XXX
 
 #define MIRYOKU_LAYOUTMAPPING_MEDIA( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
