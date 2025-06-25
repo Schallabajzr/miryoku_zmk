c00da32c47a73695620dd5677424888d607617bd
Tjaša Domadenik <tjasa.domadenik10@gmail.com>
2025-06-25 20:06:48 +0200

    test 3

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,20 +3,78 @@
 
 #define XXX &none
 #define ULK &studio_unlock
+// s with caron (š)
+ZMK_MACRO(s_caron,
+    wait-ms = <20>;
+    tap-ms = <30>;
+    bindings = <
+        &macro_press &kp RALT &kp LSHFT
+        &macro_tap &kp N6
+        &macro_release &kp RALT &kp LSHFT
+        &kp S
+    >;
+);
 
+// c with caron (č)
+ZMK_MACRO(c_caron,
+    wait-ms = <20>;
+    tap-ms = <30>;
+    bindings = <
+        &macro_press &kp RALT &kp LSHFT
+        &macro_tap &kp N6
+        &macro_release &kp RALT &kp LSHFT
+        &kp C
+    >;
+);
 
-#define S_CARON     XXX
-#define C_CARON     XXX
-#define Z_CARON     XXX
-#define O_UMLAUT    XXX
-#define U_UMLAUT    XXX
+// z with caron (ž)
+ZMK_MACRO(z_caron,
+    wait-ms = <20>;
+    tap-ms = <30>;
+    bindings = <
+        &macro_press &kp RALT &kp LSHFT
+        &macro_tap &kp N6
+        &macro_release &kp RALT &kp LSHFT
+        &kp Z
+    >;
+);
+
+// o with umlaut (ö)
+ZMK_MACRO(o_umlaut,
+    wait-ms = <20>;
+    tap-ms = <30>;
+    bindings = <
+        &macro_press &kp RALT &kp LSHFT
+        &macro_tap &kp APOS
+        &macro_release &kp RALT &kp LSHFT
+        &kp O
+    >;
+);
+
+// u with umlaut (ü)
+ZMK_MACRO(u_umlaut,
+    wait-ms = <20>;
+    tap-ms = <30>;
+    bindings = <
+        &macro_press &kp RALT &kp LSHFT
+        &macro_tap &kp APOS
+        &macro_release &kp RALT &kp LSHFT
+        &kp U
+    >;
+);
+
+#define S_CARON     &s_caron
+#define C_CARON     &c_caron
+#define Z_CARON     &z_caron
+#define O_UMLAUT    &o_umlaut
+#define U_UMLAUT    &u_umlaut
+
 
 #define MIRYOKU_LAYER_EURKEY \
 XXX,XXX,XXX,XXX,XXX,XXX,XXX,U_UMLAUT,XXX,XXX, \
 XXX,XXX,S_CARON,XXX,XXX,XXX,XXX,XXX,XXX,O_UMLAUT, \
 Z_CARON,XXX,C_CARON,XXX,XXX,XXX,XXX,XXX,XXX,XXX, \
 XXX,XXX,XXX,XXX,XXX,XXX,XXX,XXX,XXX,XXX
-
 
 #define MIRYOKU_LAYOUTMAPPING_MEDIA( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
