919498dbceefe2645cfd29a133c0574ec1cf42b1
Schallabajzr <zan.schaffer@gmail.com>
2025-06-29 18:58:58 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,6 +3,7 @@
 
 #define XXX &none
 #define ULK &studio_unlock
+#define CLEAR &bt BT_CLR
 
 #define S_CARON     &s_caron
 #define C_CARON     &c_caron
@@ -22,7 +23,7 @@ XXX,XXX,XXX,XXX,XXX,XXX,XXX,XXX,XXX,XXX
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-ULK  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
+ULK  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  CLEAR \
 XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
